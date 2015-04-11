cb38dd5a3f78444c57aa97ec57bcc45784aff015
Adnan Dželihodžić <adnandz@gmail.com>
2015-04-11 16:10:18 +0200

    prva verzija

---
+++ DAL/Baza.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class Baza:DbContext
+    {
+        public Baza() : base("ERV")
+        {
+        }
+
+        public DbSet<Uposlenik> Uposlenici { get; set; }
+        public DbSet<Dogadjaj> Dogadjaji { get; set; }
+
+
+        //za onemogucavanje automatskog dodavanja mnozine
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ DAL/Models.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+
+    //Model klasa Uposlenik ->Tabela Uposlenik u bazi podataka
+    public class Uposlenik
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Pozicija { get; set; }
+        public string Kartica { get; set; }
+        public virtual ICollection<Dogadjaj> Dogadjaji { get; set; }
+    }
+
+    public class Dogadjaj
+    {
+        public int Id {get;set;}
+        public DateTime DatumVrijemeDogadjaja {get;set;}
+        public int UposlenikId { get; set; }
+
+        public virtual Uposlenik Uposlenik { get; set; }
+    }
+}
